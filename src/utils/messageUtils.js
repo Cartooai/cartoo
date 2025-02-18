572abfb3af6f0b4f94b63142bbbfc7552177e911
josef-code <jbbliss9@gmail.com>
2025-02-18 15:24:12 +0100

    force light mode

--- src/utils/messageUtils.js
+++ src/utils/messageUtils.js
@@ -6,7 +6,7 @@ export const isProductQuery = (message) => {
         'order', 'pricing', 'discount', 'deal', 'best price', 'sale', 'coupon', 'promo code',
         'available now', 'compare','which is better', 'pros and cons', 'benefits', 'price comparison',
         'I want to get', 'I\'m looking for', 'I need', 'I want', 'I\'m interested in', 'I\'m searching for',
-        'ready to buy', 'can you help me find', 'can you help me get', 'can you help me order', 'can I get', 'I am looking',
+        'ready to buy', 'can you help me find', 'can you help me get', 'can you help me order', 'can I get', 'I am looking', 'need', 'find', 'check',
     ];
     return productKeywords.some(keyword =>
         message.toLowerCase().includes(keyword)
