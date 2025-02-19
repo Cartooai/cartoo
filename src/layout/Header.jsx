c44b0926718981eaf89b3c76626e2dab938179a0
josef-code <jbbliss9@gmail.com>
2025-02-19 10:38:44 +0100

    remove dark header from header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -37,7 +37,7 @@ function Header() {
     return (
         <>
             <Theme appearance="light">
-                <nav className="bg-white border-gray-200 dark:bg-gray-900">
+                <nav className="bg-white border-gray-200">
                     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                             <Image
